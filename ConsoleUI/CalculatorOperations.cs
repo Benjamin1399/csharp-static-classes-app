cb69464c306e2ad0f73b37ab2d089b2ee5fe3248
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-08-20 19:53:08 +0200

    Created classes
    Classes created to handle operations

---
+++ ConsoleUI/CalculatorOperations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public static class CalculatorOperations
+    {
+        public static double Add(double x, double y)
+        {
+            double output = x + y;
+
+            return output;
+        }
+
+        public static double Subtract(double x, double y)
+        {
+            double output = x - y;
+
+            return output;
+        }
+
+        public static double Divide(double x, double y)
+        {
+            double output = 0;
+
+            try
+            {
+                output = x / y;
+            }
+            catch (DivideByZeroException ex)
+            {
+                throw new DivideByZeroException("Cannot divide by zero");
+            }
+
+            return output;
+        }
+
+        public static double Multiply(double x, double y)
+        {
+            double output = x * y;
+
+            return output;
+        }
+    }
+}
